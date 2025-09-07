e767043550085d776f23d0657158b283ab363ae0
Kyle Aquino <kshaunaquino@gmail.com>
2025-09-07 23:38:30 +0800

    feat: add user admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Post
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "date_joined")
+    list_filter = ("is_staff", "is_superuser", "is_active", "date_joined")
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("-date_joined",)
