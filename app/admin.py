81d785d8de23506bf1280d5991dfdb3acdb09144
Kyle Aquino <kshaunaquino@gmail.com>
2025-09-07 23:39:07 +0800

    feat: add post model

--- app/admin.py
+++ app/admin.py
@@ -8,3 +8,23 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ("is_staff", "is_superuser", "is_active", "date_joined")
     search_fields = ("username", "first_name", "last_name", "email")
     ordering = ("-date_joined",)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "is_published", "published_at", "created_at")
+    list_filter = ("is_published", "created_at", "published_at", "author")
+    search_fields = ("title", "content", "excerpt", "author__username")
+    ordering = ("-created_at",)
+    prepopulated_fields = {"slug": ("title",)}
+
+    fieldsets = (
+        ("Basic Information", {"fields": ("title", "slug", "author")}),
+        ("Content", {"fields": ("excerpt", "content")}),
+        ("Publishing", {"fields": ("is_published", "published_at")}),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not change:  # If creating a new post
+            obj.author = request.user
+        super().save_model(request, obj, form, change)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -19,3 +21,33 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Post(models.Model):
+    """
+    Post model.
+    """
+
+    title = models.CharField(max_length=200, help_text="Post title")
+    slug = models.SlugField(max_length=200, unique=True, help_text="URL-friendly version of title")
+    content = models.TextField(help_text="Post content")
+    excerpt = models.TextField(max_length=300, blank=True, help_text="Short excerpt for preview")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True, help_text="When the post was published")
+    is_published = models.BooleanField(default=False, help_text="Whether the post is published")
+
+    class Meta:
+        ordering = ["-published_at", "-created_at"]
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if self.is_published and not self.published_at:
+            self.published_at = timezone.now()
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("app:post_detail", kwargs={"slug": self.slug})
